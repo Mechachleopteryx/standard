9836ccdecdd9595d9f78eb9f22ee028526876436
Deep Patel <deepp@uber.com>
2017-04-07 11:05:29 -0400

    Split files and run eslint on them in separate processes to utilize multiple cores

--- test/api.js
+++ test/api.js
@@ -1,11 +1,17 @@
-var standard = require('../')
-var test = require('tape')
+'use strict';
 
-test('api usage', function (t) {
-  t.plan(3)
-  standard.lintFiles([], { cwd: 'bin' }, function (err, result) {
-    t.error(err, 'no error while linting')
-    t.equal(typeof result, 'object', 'result is an object')
-    t.equal(result.errorCount, 166, 'error count 166')
-  })
-})
+var standard = require('../index.js');
+var test = require('tape');
+
+test('api usage', function t(assert) {
+    standard.lintFiles([], {
+        cwd: 'bin'
+    }, function onFiles(err, result) {
+        assert.error(err, 'no error while linting');
+        assert.equal(typeof result, 'object', 'result is an object');
+        assert.equal(result.errorCount, 166, 'error count 166');
+
+        assert.end();
+    });
+});
+
