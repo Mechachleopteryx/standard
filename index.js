c83cbbedf671c86fb4a1a7e9cce4d20e2c4bb5f0
Deep Patel <deepp@uber.com>
2017-04-07 11:05:29 -0400

    Split files and run eslint on them in separate processes to utilize multiple cores

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ var glob = require('glob')
 var parallel = require('run-parallel')
 var path = require('path')
 var uniq = require('uniq')
+var cloneDeep = require('clone-deep')
+var deepExtend = require('deep-extend')
+var fs = require("fs")
+var path = require("path")
+var cp = require('child_process')
+var _ = require('lodash')
+
+var NUM_FORKS = 8;
 
 var DEFAULT_PATTERNS = [
   '**/*.js',
@@ -120,16 +128,47 @@ function lintFiles (files, opts, cb) {
     var root = commondir(files)
     editorConfigGetIndent(root, function (err, indent) {
       if (err) return cb(err)
-      var result
-      ESLINT_CONFIG.baseConfig.rules.indent = [2, indent]
       try {
-        result = new eslint.CLIEngine(ESLINT_CONFIG).executeOnFiles(files)
+        executeInParallel(files, ESLINT_CONFIG, cb)
       } catch (err) {
         return cb(err)
       }
-      return cb(null, result)
     })
 
+  })
+}
+
+function executeInParallel(files, opts, cb) {
+  var chunkResults = {};
+  var fileChunks = _.chunk(files, Math.ceil(files.length/NUM_FORKS))
+  var pendingResults = fileChunks.length;
+  var results = { results: [], errorCount: 0, warningCount: 0 }
+
+  function onAllDone() {
+    fileChunks.forEach(function(chunk) {
+      results.results = results.results.concat(chunkResults[chunk].results)
+      results.errorCount += chunkResults[chunk].errorCount
+      results.warningCount += chunkResults[chunk].warningCount
+    })
+    cb(null, results)
+  }
+
+  fileChunks.forEach(function(chunk, i) {
+    var fork = cp.fork(__dirname + '/executeOnFiles')
+    fork.send(opts)
+    fork.send(chunk)
+    fork.send('end')
+    fork.on('message', function(results) {
+      if(results.isError) {
+        return cb(new Error(result.error))
+      }
+
+      chunkResults[chunk] = results
+      pendingResults--
+      if(pendingResults == 0) {
+        onAllDone()
+      }
+    })
   })
 }
 
--- test/api.js
+++ test/api.js
@@ -1,3 +1,8 @@
+'use strict';
+
+var standard = require('../index.js');
+var test = require('tape');
+
 var standard = require('../')
 var test = require('tape')
 
@@ -9,3 +14,38 @@ test('api usage', function (t) {
     t.equal(result.errorCount, 166, 'error count 166')
   })
 })
+
+test('api usage', function t(assert) {
+    standard.lintFiles([], {
+        cwd: 'bin'
+    }, function onFiles(err, result) {
+        assert.error(err, 'no error while linting');
+        assert.equal(typeof result, 'object', 'result is an object');
+        assert.equal(result.errorCount, 181, 'error count 181');
+
+        assert.end();
+    });
+});
+
+test('lintText', function t(assert) {
+    standard.lintText(
+        '\'use strict\';\n' +
+        'try {\n' +
+        '    var foo = 1 / 0;\n' +
+        '    throw foo;\n' +
+        '} catch (e) {\n' +
+        '    throw e;\n' +
+        '}\n',
+        onLint
+    );
+
+    function onLint(err, results) {
+        assert.ifError(err);
+
+        var messages = results.results[0].messages;
+        assert.equal(messages[0].ruleId, 'no-try-catch');
+
+        assert.end();
+    }
+});
+>>>>>>> 82cef5d... Split files and run eslint on them in separate processes to utilize multiple cores
